63a1b1835c8ae2a4cee184b2f9e51a4454772d9d
rofinafiin <rofinafiisr@gmail.com>
2023-01-10 07:11:05 +0700

    Tinggal get data sama implement edit aja

--- BlazorApp2/Server/Controllers/BukuController.cs
+++ BlazorApp2/Server/Controllers/BukuController.cs
@@ -10,75 +10,46 @@ namespace BlazorApp2.Server.Controllers
     [ApiController]
     public class BukuController : ControllerBase
     {
-
-        public static List<Genre> Genre = new List<Genre>
-        {
-            new Genre
-            {
-                Genre_id = 1,
-                Nama_Genre = "Horor"
-            },
-            new Genre
-            {
-                Genre_id = 2,
-                Nama_Genre = "Comedi"
-            },
-            new Genre
-            {
-                Genre_id = 3,
-                Nama_Genre = "Romance"
-            },
-            new Genre
-            {
-                Genre_id = 4,
-                Nama_Genre = "Fantasi"
-            },
-            new Genre
-            {
-                Genre_id = 5,
-                Nama_Genre = "Edukasi"
-            }
-        };
-        public static List<Buku> Buku = new List<Buku>
-        {
-            new Buku{ Id_buku = 11, Judul_buku = "Dear Nathan", 
-                Penerbit = "Tere", Jumlah_halaman = 250, Genre = Genre[0], Genre_id = 1},
-            new Buku{ Id_buku = 12, Judul_buku = "Dilan 1991",
-                Penerbit = "Pidi Baiq", Jumlah_halaman = 250, Genre = Genre[1], Genre_id = 2},
-            new Buku{ Id_buku = 16, Judul_buku = "Dibalik 98",
-                Penerbit = "Ismail", Jumlah_halaman = 250, Genre = Genre[2], Genre_id = 3}
-        };
+        private readonly DataContext _context;
+        public BukuController(DataContext context) 
+        { 
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Buku>>> GetDataBuku()
         {
+            var Buku = await _context.Buku.ToListAsync();
             return Ok(Buku);
         }
         [HttpGet("genre")]
         public async Task<ActionResult<List<Buku>>> GetGenre()
         {
+            var Genre = await _context.Genre.ToListAsync();
             return Ok(Genre);
         }
 
-            [HttpGet("{id}")]
-        public async Task<ActionResult<Buku>> GetSingleDataBuku(int id)
-        {
-            var buku = Buku.FirstOrDefault(b => b.Id_buku == id);
-            if (buku == null)
-            {
-                return NotFound("Data Buku Tidak Ada :/");
-            }
-            return Ok(buku);
-        }
-        [HttpGet("genre{id}")]
-        public async Task<ActionResult<Buku>> GetSingleGenre(int id)
-        {
-            var genre = Genre.FirstOrDefault(b => b.Genre_id == id);
-            if (genre == null)
-            {
-                return NotFound("Data Buku Tidak Ada :/");
-            }
-            return Ok(genre);
-        }
+        //    [HttpGet("{id}")]
+        //public async Task<ActionResult<Buku>> GetSingleDataBuku(int id)
+       // {
+          //  var buku = await _context.Buku
+            //    .Include(b => b.Genre)
+              //  .FirstOrDefault(b => b.Id_buku == id);
+            //if (buku == null)
+            //{
+              //  return NotFound("Data Buku Tidak Ada :/");
+            //}
+            //return Ok(buku);
+       // }
+       // [HttpGet("genre{id}")]
+        //public async Task<ActionResult<Buku>> GetSingleGenre(int id)
+        //{
+          //  var genre =await  Genre.FirstOrDefault(b => b.Genre_id == id);
+            //if (genre == null)
+            //{
+             //   return NotFound("Data Buku Tidak Ada :/");
+            //}
+            //return Ok(genre);
+        //}
     }
 }
